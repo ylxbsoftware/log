2826c51b487ab9d2850cec4b134a6c61fdf8ebad
wanbao <ylxbsoftware@gmail.com>
2016-07-08 18:14:21 +0800

    优化打包脚本

--- zip.py
+++ zip.py
@@ -5,9 +5,13 @@
 #git打包仅导出不同提交记录中被更新过的文件
 
 import os
-new = str.strip(raw_input('请输入new-commit-id(不输入默认为HEAD): '));
+
+name = str.strip(raw_input('请输入最终打包名称(默认为update): '));
+new = str.strip(raw_input('请输入new-commit-id(默认为HEAD): '));
 old = str.strip(raw_input('请输入old-commit-id: '));
+if(len(name)==0):
+  name = 'update';
+
 if(len(new)==0):
   new = 'HEAD'
-
-os.system('git archive -o update.zip ' + new +' $(git diff --name-only --diff-filter=ACMRT ' + old + ' ' + new + ')')
+os.system('git archive -o ' + name + '.zip ' + new +' $(git diff --name-only --diff-filter=ACMRT ' + old + ' ' + new + ')')
